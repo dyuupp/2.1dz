ccb1cb4ec4b50fd11c2eff06cfbcec5a18e91e44
dyuupp <rizosh@yandex.ru>
2022-05-17 11:18:21 +0300

    added files

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,6 +2,6 @@ public class BonusMilesService {
     public int calculate(int price) {
         int bonus = 20;
         int total = price / bonus;
-          return(total);
+        return total;
     }
 }
